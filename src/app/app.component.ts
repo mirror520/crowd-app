57566f3fcfaf175649c2f8aee4bda0262c2d1f4d
Lin, Ying-Chin <mirror770109@gmail.com>
2021-05-22 12:51:10 +0800

    Login by query parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { MqttService } from './mqtt.service';
 import { User } from './model/user';
@@ -8,20 +10,33 @@ import { User } from './model/user';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'crowd-app';
-  user: User;
+  mqttSubscription: Subscription;
 
   constructor(
+    private activatedRoute: ActivatedRoute,
     private mqttService: MqttService
   ) {
-    this.user = new User();
-    this.user.username = 'username';
-    this.user.password = 'password';
+    const user = new User();
+    user.username = this.activatedRoute.snapshot.paramMap.get('username');
+    user.password = this.activatedRoute.snapshot.paramMap.get('password');
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (params['username'] && params['password']) {
+        const user = new User();
+        user.username = params['username'];
+        user.password = params['password'];
+
+        this.connectMqtt(user);
+      }
+    });
   }
 
-  ngOnInit() {
-    this.mqttService.connect(this.user).subscribe({
+  connectMqtt(user: User) {
+    if (this.mqttSubscription) return;
+
+    this.mqttSubscription = this.mqttService.connect(user).subscribe({
       next: (value) => console.log(`MQTT Service: ${value}`),
       error: (err) => console.error(err),
       complete: () => console.log('complete')
